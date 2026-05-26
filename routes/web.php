6b67d7b3ccd6ac9c3204e793f4403884908860ab
indra <irawanindra405@gmail.com>
2026-05-26 15:51:05 +0700

    setup firebase

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Kreait\Firebase\Factory;
+use Google\Cloud\Firestore\FirestoreClient;
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/firebase-test', function () {
+
+    putenv('GOOGLE_APPLICATION_CREDENTIALS=' . storage_path('app/firebase/firebase_credentials.json'));
+
+    $firestore = new FirestoreClient([
+        'projectId' => 'smart-composting',
+    ]);
+
+    return 'Firebase Connected Successfully';
 });
